13324308f50d0b25dc95bdd3e7d2a2640c672b9e
Bianca Simion <Bianca.Simion@matchesfashion.com>
2017-07-17 17:12:19 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  //get all the todos
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
   
 app.listen(3000, () => {
