1dc8580e2e6d8681b6a49c45dde98a5d5594ef41
Bianca Simion <Bianca.Simion@matchesfashion.com>
2017-07-19 12:10:37 +0100

    Test the DELETE /todos:id route

--- server/server.js
+++ server/server.js
@@ -67,11 +67,11 @@ app.delete('/todos/:id', (req, res) => {
   };
 
   //remove todo by id 
-  Todo.findByIdAndRemove(id).then((doc) => {
-    if (!doc) {
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
-    res.send(doc);
+    res.send({todo});
   }).catch((e) =>{
     res.status(400).send();
   });
